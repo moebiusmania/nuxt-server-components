13e527d74ae053b1a79a9192f6380a8029c9b6ef
Salvatore Laisa <salvatore.laisa@casavo.com>
2023-10-27 19:16:59 +0200

    chore: first commit

---
+++ app.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const list = [
+  { label: "Halloween", date: "10-31-2023" },
+  { label: "NYE", date: "01-01-2024" },
+]
+
+const current = ref(0)
+const buttonLabel = current.value === 0 ? list[1].label : list[0].label
+
+const switchDate = () => {
+  current.value = current.value === 0 ? 1 : 0
+}
+</script>
+
+<template>
+  <div>
+    <h1>Date countdown app</h1>
+    <Countdown :label="list[current].label" :date="list[current].date" />
+    <button @click="switchDate">Switch to {{ buttonLabel }} </button>
+  </div>
+</template>
---
+++ components/Countdown.server.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { formatDistance } from 'date-fns'
+
+const {
+  label,
+  date,
+} = defineProps<{
+  label: string
+  date: string
+}>()
+
+const target = new Date(date)
+const result = formatDistance(new Date(), target)
+</script>
+
+<template>
+  <p>Days to {{ label }}: <strong>{{ result }}</strong></p>
+</template>
