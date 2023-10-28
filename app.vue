27bc157a369d762a715cb8b880211233fc09edc0
moebiusmania <moebiusmania@gmail.com>
2023-10-28 12:54:10 +0200

    feat: switch to ASCII faces

--- app.vue
+++ app.vue
@@ -1,23 +1,21 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 
-const list = [
-  { label: "Halloween", date: "10-31-2023" },
-  { label: "NYE", date: "01-01-2024" },
-]
-
 const current = ref(0)
-const buttonLabel = current.value === 0 ? list[1].label : list[0].label
 
-const switchDate = () => {
-  current.value = current.value === 0 ? 1 : 0
+const updateFace = () => {
+  current.value = Math.floor(Math.random() * 277)
 }
 </script>
 
 <template>
-  <div>
-    <h1>Date countdown app</h1>
-    <Countdown :label="list[current].label" :date="list[current].date" />
-    <button @click="switchDate">Switch to {{ buttonLabel }} </button>
-  </div>
+  <main class="container">
+    <h1>ASCII faces app</h1>
+    <Faces :index="current" />
+    <button @click="updateFace">Pick another one...</button>
+  </main>
 </template>
+
+<style>
+@import '@picocss/pico';
+</style>
---
+++ components/Faces.server.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import ascii from 'ascii-faces'
+
+const {
+  index = 0
+} = defineProps<{
+  index: number
+}>()
+
+</script>
+
+<template>
+  <p>Here is some cool ASCII face!</p>
+  <pre><code>{{ ascii.faces[index] }}</code></pre>
+</template>
